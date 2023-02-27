3ef1824d64530be1f2804c02547efa0513f57bdf
amanda ranero <amandaranero@gmail.com>
2023-02-27 17:55:40 -0500

    redo on my own

--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
+
 document.addEventListener("DOMContentLoaded", fetchPups)
     
 
 function fetchPups(){
     fetch("http://localhost:3000/pups")
-        .then(resp => resp.json())
+        .then(response => response.json())
         .then(pups => addAllPups(pups))
 }
 
 function addAllPups(pups){
-    const dogBar = document.getElementById("dog-bar")
-    pups.forEach((pup)=>{
-       const newDogSpan = document.createElement("span")
-       newDogSpan.innerText = pup.name
-       newDogSpan.dataset.id = pup.id
-       dogBar.append(newDogSpan)
-       
+   const dogBar = document.getElementById("dog-bar")
+   pups.forEach((pup)=>{
+    const newDogSpan = document.createElement("span")
+    newDogSpan.innerText = pup.name
+    newDogSpan.dataset.id = pup.id
+    dogBar.append(newDogSpan)
 
-       newDogSpan.addEventListener("click", handlePupClick)
-       
-    })
+    newDogSpan.addEventListener("click", pupClick)
+   })
 }
 
-function handlePupClick(e){
+function pupClick(e){
     fetch(`http://localhost:3000/pups/${e.target.dataset.id}`)
-        .then(resp=> resp.json())
-        .then(pupData => addOnePup(pupData))
-}
+        .then((resp)=> resp.json())
+        .then((pupData)=> addOnePup(pupData))
+};
+
 function addOnePup(pup){
     const dogInfo = document.getElementById("dog-info")
     dogInfo.innerHTML = ""
-
+    
     const pupImage = document.createElement("img")
     pupImage.src = pup.image
-    
 
     const pupName = document.createElement("h2")
     pupName.innerText = pup.name
 
-    const pupButton = document.createElement("button")
-    pupButton.innerText = pup.isGoodDog ? "Good Dog!" : "Bad Dog!"
-    pupButton.addEventListener("click", handlePupButton)
+    const goodOrBadButton = document.createElement("button")
+    goodOrBadButton.innerText = pup.isGoodDog ? "Good Dog!" : "Bad Dog!"
+    goodOrBadButton.addEventListener("click", goodOrBadButtonClick)
 
-    dogInfo.append(pupImage,pupName, pupButton)
-}
+    dogInfo.append(pupImage, pupName, goodOrBadButton)
 
-function handlePupButton(e){
-    if (e.target.innerText === "Good Dog!"){
-        e.target.innerText = "Bad Dog!"
     }
-    else {
-        e.target.innerText = "Good Dog!"
+
+    function goodOrBadButtonClick(e){
+        if (e.target.innerText === "Good Dog!"){
+            e.target.innerText = "Bad Dog!"
+        }
+        else {
+            e.target.innerText = "Good Dog!"
+        }
     }
-}
