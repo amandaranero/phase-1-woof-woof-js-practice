da72c924e157490baa59a53f1b3c7c39a872e1e1
amanda ranero <amandaranero@gmail.com>
2023-02-28 12:28:16 -0500

    third try, with bonus, DJ run through

--- src/index.js
+++ src/index.js
@@ -1,54 +1,75 @@
 
-document.addEventListener("DOMContentLoaded", fetchPups)
-    
-
-function fetchPups(){
+const run= () => {
     fetch("http://localhost:3000/pups")
-        .then(response => response.json())
-        .then(pups => addAllPups(pups))
-}
-
-function addAllPups(pups){
-   const dogBar = document.getElementById("dog-bar")
-   pups.forEach((pup)=>{
-    const newDogSpan = document.createElement("span")
-    newDogSpan.innerText = pup.name
-    newDogSpan.dataset.id = pup.id
-    dogBar.append(newDogSpan)
-
-    newDogSpan.addEventListener("click", pupClick)
-   })
-}
-
-function pupClick(e){
-    fetch(`http://localhost:3000/pups/${e.target.dataset.id}`)
         .then((resp)=> resp.json())
-        .then((pupData)=> addOnePup(pupData))
-};
+        .then((dogData) => {
+            const goodDogFilter = document.getElementById("good-dog-filter")
+            const dogBar = document.getElementById("dog-bar")
 
-function addOnePup(pup){
+            goodDogFilter.addEventListener("click", ()=>{
+             if (goodDogFilter.innerText === "Filter good dogs: OFF"){
+                goodDogFilter.innerText = "Filter good dogs: ON"
+                dogBar.innerText = ""
+                const filterPups = dogData.filter((pup)=>{
+                    if(pup.isGoodDog){
+                        return pup;
+                    }
+                })
+            filterPups.forEach((pup)=>{
+                addDogToNavBar(pup)
+            })
+
+             } else {
+                goodDogFilter.innerText = "Filter good dogs: OFF"
+                dogBar.innerText = ""
+                dogData.forEach((pup)=>{
+                    addDogToNavBar(pup)
+                })
+                
+             }
+            })
+            dogData.forEach((pup)=>{
+            addDogToNavBar(pup)
+      
+        })
+})
+}
+
+
+function addDogToNavBar(pupObj){
+    const dogBar = document.getElementById("dog-bar")
     const dogInfo = document.getElementById("dog-info")
-    dogInfo.innerHTML = ""
+ 
+
+    const dogSpan = document.createElement("span")
+    dogSpan.innerText = pupObj.name
+    dogBar.append(dogSpan)
+
+    dogSpan.addEventListener("click", ()=>{
+        dogInfo.innerHTML = ""
+        const dogImg = document.createElement("img")
+        dogImg.src = pupObj.image
+
+        const dogName = document.createElement("h2")
+        dogName.innerText = pupObj.name
+
+        const dogButton = document.createElement("button")
+        dogButton.innerText = pupObj.isGoodDog ? "Good Dog!" : "Bad Dog!"
+
+        dogInfo.append(dogImg , dogName, dogButton)
+
+        dogButton.addEventListener("click", ()=>{
+            if (dogButton.innerText === "Good Dog!"){
+                (dogButton.innerText = "Bad Dog!")
+            }
+            else {dogButton.innerText = "Good Dog!"}
+        })
+    })
+      
     
-    const pupImage = document.createElement("img")
-    pupImage.src = pup.image
+}
 
-    const pupName = document.createElement("h2")
-    pupName.innerText = pup.name
 
-    const goodOrBadButton = document.createElement("button")
-    goodOrBadButton.innerText = pup.isGoodDog ? "Good Dog!" : "Bad Dog!"
-    goodOrBadButton.addEventListener("click", goodOrBadButtonClick)
 
-    dogInfo.append(pupImage, pupName, goodOrBadButton)
 
-    }
-
-    function goodOrBadButtonClick(e){
-        if (e.target.innerText === "Good Dog!"){
-            e.target.innerText = "Bad Dog!"
-        }
-        else {
-            e.target.innerText = "Good Dog!"
-        }
-    }
+document.addEventListener('DOMContentLoaded', run)
