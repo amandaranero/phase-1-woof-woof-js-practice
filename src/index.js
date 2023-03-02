d5a9d15392aa7a4bf274cb461e8d045b42f8e9ab
amanda ranero <amandaranero@gmail.com>
2023-03-01 22:58:57 -0500

    another try, this time with forEach in the .then, much easier

--- src/index.js
+++ src/index.js
@@ -1,40 +1,95 @@
 
-document.addEventListener("DOMContentLoaded", run())
+document.addEventListener("DOMContentLoaded", run)
 
 function run(){
-    fetch("http://localhost:3000/pups")
-        .then((resp)=> resp.json())
-        .then((pupsData)=> main(pupsData))
-function main(data){
- //add span to div bar
-  const dogBar = document.getElementById("dog-bar")
 
-  data.forEach((dog)=>{
-    const dogSpan = document.createElement("span")
-    dogSpan.innerText = dog.name
+    //getting dog data
+fetch ("http://localhost:3000/pups")
+    .then((resp)=> resp.json())
+    .then((pups) => {
+        pups.forEach(addPup)
+    })
 
-    dogSpan.addEventListener("click", ()=>{
+    //add pup function, pup is the object
+    function addPup(dog){
+    //adding a span with the text name and appending it to dog bar
+        const dogBar  = document.getElementById("dog-bar")
+        const dogName = document.createElement("span")
+        dogName.innerText = dog.name
+        dogBar.append(dogName)
+
+        //add dog info, need click event on dogName
+    dogName.addEventListener("click", ()=>{
         const dogInfo = document.getElementById("dog-info")
-        dogInfo.innerHTML = ""
+        dogInfo.innerHTML = ''
 
         const dogImage = document.createElement("img")
         dogImage.src = dog.image
-        
-        const dogName = document.createElement("h2")
-        dogName.innerText = dog.name
 
-        const button = document.createElement("button")
-        button.innerText = dog.isGoodDog ? "Good Dog!" : "Bad Dog!"
+        const dogInfoName = document.createElement("h2")
+        dogInfoName.innerText = dog.name
 
-        dogInfo.append(dogImage, dogName, button)
+        const dogButton = document.createElement("button")
+        dogButton.innerText = dog.isGoodDog ? "Good Dog!" : "Bad Dog!"
 
-        button.addEventListener("click", ()=>{
-            if(button.innerText === "Good Dog!"){
-                button.innerText = "Bad Dog!"
-            } else { button.innerText = "Good Dog!"}
-        })
+      //toggle dog button
+      dogButton.addEventListener("click", ()=>{
+        if (dogButton.innerText === "Good Dog!"){
+            dogButton.innerText = "Bad Dog!"
+        } else {
+            dogButton.innerText = "Good Dog!"
+        }
+      })
+
+        dogInfo.append(dogImage, dogInfoName, dogButton)
     })
-  dogBar.append(dogSpan)
-  })
-}  
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+// function run(){
+//     fetch("http://localhost:3000/pups")
+//         .then((resp)=> resp.json())
+//         .then((pupsData)=> main(pupsData))
+// function main(data){
+//  //add span to div bar
+//   const dogBar = document.getElementById("dog-bar")
+
+//   data.forEach((dog)=>{
+//     const dogSpan = document.createElement("span")
+//     dogSpan.innerText = dog.name
+
+//     dogSpan.addEventListener("click", ()=>{
+//         const dogInfo = document.getElementById("dog-info")
+//         dogInfo.innerHTML = ""
+
+//         const dogImage = document.createElement("img")
+//         dogImage.src = dog.image
+        
+//         const dogName = document.createElement("h2")
+//         dogName.innerText = dog.name
+
+//         const button = document.createElement("button")
+//         button.innerText = dog.isGoodDog ? "Good Dog!" : "Bad Dog!"
+
+//         dogInfo.append(dogImage, dogName, button)
+
+//         button.addEventListener("click", ()=>{
+//             if(button.innerText === "Good Dog!"){
+//                 button.innerText = "Bad Dog!"
+//             } else { button.innerText = "Good Dog!"}
+//         })
+//     })
+//   dogBar.append(dogSpan)
+//   })
+// }  
+// }
